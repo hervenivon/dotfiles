72d294452ecb0105fc407d831f1fbbc9c8dd3cae
Hervé Nivon <herve.nivon@gmail.com>
2023-03-26 16:10:28 +0200

    fix: remove gpg dependency

--- bootstrap.sh
+++ bootstrap.sh
@@ -281,10 +281,6 @@ brew_cleanup () {
 }
 
 install_rvm () {
-  curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-  curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
-  echo 409B6B1796C275462A1703113804BB82D39DC0E3:6: | gpg --import-ownertrust
-  echo 7D2BAF1CF37B13E2069D6956105BD0E739499BDB:6: | gpg --import-ownertrust
   curl -sSL https://get.rvm.io | bash -s stable --ruby
 }
 
